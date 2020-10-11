8ab513892f7b3ddeb1e32a00eff6e274f1f52eaa
Peba21 <kdg0676@gmail.com>
2020-10-11 08:44:44 +0300

    // kazyra

---
+++ database/sql/core_applicationsref.sql
@@ -0,0 +1,8 @@
+INSERT INTO core_applicationsref (app_id, app_code, app_name, app_url) 
+	VALUES (100, 'LOAN.100', 'APP_NAME', 'www.blas-bla');
+INSERT INTO core_applicationsref (app_id, app_code, app_name, app_url) 
+	VALUES (1001, 'USERS', 'Приложение юзерс', 'security-manager/user.xhtml');
+INSERT INTO core_applicationsref (app_id, app_code, app_name, app_url) 
+	VALUES (102, 'lc.fl', 'Кредиты подозрительным лицам', 'retail-loan-contracts/Loan2Individual.xhtml');
+INSERT INTO core_applicationsref (app_id, app_code, app_name, app_url) 
+	VALUES (1010, 'APP.13', 'Кредиты физическим лицам', 'retail-loan-contracts/loans2fl');
